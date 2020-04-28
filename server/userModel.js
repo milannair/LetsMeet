9db6d49823fdefd6b02ab5163bfb2eed34d2032d
Milan Nair <milannair@Milans-MBP.fios-router.home>
2020-04-28 16:31:10 -0700

    skeleton

---
+++ server/userModel.js
@@ -0,0 +1,22 @@
+var mongoose = require("mongoose");
+// Setup schema
+var userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: String,
+  create_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+// Export user model
+var User = (module.exports = mongoose.model("user", userSchema));
+module.exports.get = function (callback, limit) {
+  User.find(callback).limit(limit);
+};
