1f4f795e38adeec504d4deb25eed306abeaf46e1
Admin <nelson.tan7@gmail.com>
2020-05-11 13:42:14 -0700

    moved all components to SignupScreen.js

--- frontend/screens/SignupScreen.js
+++ frontend/screens/SignupScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import EmailFieldComponent from '../components/EmailFieldComponent';
+import { TextInput, Button } from 'react-native-paper';
+// import EmailFieldComponent from '../components/EmailFieldComponent';
 import PasswordFieldComponent from '../components/PasswordFieldComponent';
 import NameFieldComponent from '../components/NameFieldComponent';
 import CreateAccountComponent from '../components/CreateAccountComponent';
@@ -10,28 +11,64 @@ function SignupScreen() {
     <View style={styles.container}>
       <Text style={styles.text}>LetsMeet</Text>
       <Text style={styles.text}>Create Your Account</Text>
-      <NameFieldComponent text="Display Name" />
-      <NameFieldComponent text="Username" />
-      <EmailFieldComponent />
-      <PasswordFieldComponent text="Password" />
-      <PasswordFieldComponent text="Confirm Password" />
-      <CreateAccountComponent />
+      <TextInput // display name field
+        style={styles.textField}
+        label="DisplayName"
+      />
+      <TextInput // username field
+        style={styles.textField}
+        label="Username"
+      />
+      <TextInput // email field
+        style={styles.textField}
+        label="Email"
+        autoCompeleteType="email"
+        keyboardType="email-address"
+        textContentType="emailAddress"
+        // onChange={handleChangeText}
+      />
+      <TextInput // password field
+        style={styles.textField}
+        label="Password"
+        secureTextEntry
+        autoCorrect={false}
+      />
+      <TextInput // confirm password field
+        style={styles.textField}
+        label="Confirm Password"
+        secureTextEntry
+        autoCorrect={false}
+      />
+      <Button // create account button
+        // onPress={handleButtonPress}
+        style={styles.button}
+        mode="contained"
+      >
+        Create Account
+      </Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    margin: 10,
-    paddingTop: '10%',
+    marginTop: 10,
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'stretch',
+    alignItems: 'center',
   },
   text: {
     textAlign: 'center',
     fontSize: 25,
+  },
+  textField: {
+    marginVertical: 15,
+    width: '90%',
+    justifyContent: 'center',
+  },
+  button: {
+    marginVertical: 15,
   },
 });
 
