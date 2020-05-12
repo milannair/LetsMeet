7d9bf60027bb214c300f8697a328c4210b548fa7
Admin <nelson.tan7@gmail.com>
2020-05-12 15:08:37 -0700

    set theme

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import ReferenceScreen from './screens/reference_screen/index';
 import SignupScreen from './screens/SignupScreen/index';
 import Constants from 'expo-constants';
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#b57edc',
+    accent: '#aaf0d1',
+  },
+};
+
 export default function App() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <View style={styles.container}>
         <SignupScreen />
       </View>
