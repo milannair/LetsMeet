38e501649662d51e597b9d7c38bad1e5d2b55d14
Leo <liaoyuan@gmail.com>
2020-05-13 10:33:27 -0700

    Add tests for the notifications screen

---
+++ frontend/__tests__/NotificationComponent.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NotificationComponent from '../components/NotificationComponent';
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <NotificationComponent
+        text={"Test notification"}
+        positiveAction={() => console.log("+")}
+        negativeAction={() => console.log("-")}
+      />
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ frontend/__tests__/NotificationsScreen.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NotificationsScreen from "../screens/NotificationsScreen";
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <NotificationsScreen
+        notifications={["Dummy notification 1", "Dummy notification 2"]}
+      />
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
