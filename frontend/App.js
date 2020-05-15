4b034dca3727a282d87b1179a52262f982e6cb1c
ratanap <ratanap@uw.edu>
2020-05-14 18:30:20 -0700

    Added hearts to the group cards. Cards inform the parent when a heart-status is changed

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
-import FillerScreen from './screens/FillerScreen';
 import GroupsPage from './screens/GroupsPage'
 import Constants from 'expo-constants';
 
@@ -9,7 +8,6 @@ export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
-        {/* <FillerScreen /> */}
         <GroupsPage/>
       </View>
     </PaperProvider>
