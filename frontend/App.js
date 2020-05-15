aa7ec4f16eb5c550f8bbebda6c9b157b77cff4b0
ratanap <ratanap@uw.edu>
2020-05-14 18:40:54 -0700

    Deleted commented out code and renamed a screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
-import GroupsPage from './screens/GroupsPage'
+import GroupsScreen from './screens/GroupsScreen'
 import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <GroupsPage/>
+        <GroupsScreen/>
       </View>
     </PaperProvider>
   );
