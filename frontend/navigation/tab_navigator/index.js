8040515fda52cd10bd68d6b7b50aa2f2b1f3d66e
Brandon Fok <fokb@uw.edu>
2020-05-17 00:04:46 -0700

    add notifications to bottom tab navbar

--- frontend/navigation/tab_navigator/index.js
+++ frontend/navigation/tab_navigator/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import * as Screen from './screen-names';
 import MeetingsStackScreen from './stacks/meetings/index';
 import GroupsStackScreen from './stacks/groups/index';
+import NotificationsScreen from '../../screens/NotificationsScreen/index';
 import ProfileStackScreen from './stacks/profile/index';
 
 const Tab = createMaterialBottomTabNavigator();
 
+// TODO: use useTheme to grab colors, instead of hardcoding colors
 function TabNavigator() {
   return (
     <Tab.Navigator
@@ -32,6 +35,16 @@ function TabNavigator() {
           tabBarColor: '#b57edc',
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name='account-multiple' color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name={Screen.NOTIFICATIONS}
+        component={NotificationsScreen}
+        options={{
+          tabBarColor: '#b57edc',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name='notifications' color={color} size={24} />
           ),
         }}
       />
--- frontend/navigation/tab_navigator/screen-names.js
+++ frontend/navigation/tab_navigator/screen-names.js
@@ -2,3 +2,4 @@ export const REFERENCE = "Reference";
 export const MEETINGS = "Meetings";
 export const GROUPS = "Groups";
 export const PROFILE = "Profile";
+export const NOTIFICATIONS = "Notifications";
