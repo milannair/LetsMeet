2952567605bc840f1866651a1815433b2850cf75
Milan Nair <milannair99@gmail.com>
2020-05-18 09:30:49 -0700

    changed default username and password

--- init.sh
+++ init.sh
@@ -16,9 +16,9 @@ else
     npm install
     cd ..
     echo ''
-    echo 'Remember to replace DB_USERNAME and DB_PW placeholders in api/.env with your unique DB Credentials. 
-Then, run this script again (bash init.sh) to get your unique connection string.'
-    open api/.env
+    #echo 'Remember to replace DB_USERNAME and DB_PW placeholders in api/.env with your unique DB Credentials. 
+#Then, run this script again (bash init.sh) to get your unique connection string.'
+    #open api/.env
     echo ''
     echo 'To install React Native expo-cli globally, run: 
     sudo npm install -g expo-cli' 
