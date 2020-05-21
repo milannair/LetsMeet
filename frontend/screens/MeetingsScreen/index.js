8632e97915a16cd451b076092b21b5c5fe5782b7
Nan Lin <Sherrylinnan@gmail.com>
2020-05-20 23:25:53 -0700

    update MeetingsScreen

--- frontend/screens/MeetingsScreen/index.js
+++ frontend/screens/MeetingsScreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View} from 'react-native';
 import { Appbar, List} from 'react-native-paper';
 import styles from './styles';
 
