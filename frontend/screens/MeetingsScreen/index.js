5acac81109ef7da82c1d08c731647858530b873b
Nan Lin <Sherrylinnan@gmail.com>
2020-05-20 23:00:53 -0700

    update MeetingsScreen

--- frontend/screens/MeetingsScreen/index.js
+++ frontend/screens/MeetingsScreen/index.js
@@ -6,7 +6,48 @@ import styles from './styles';
 
 import { TabView, SceneMap } from 'react-native-tab-view';
 
+const FirstRoute = () => (
+    <List.Section>
+        <List.Subheader>Tuesday, May 5th</List.Subheader>
+        <List.Item
+        title="CSE 403 GROUP(group name)"
+        />
+        <List.Item
+        title="Team Meeting(meeting name)"
+        />
+        <List.Item
+        title="3:00 PM - 7:00 PM"
+        />
+    </List.Section>
+  );
+
+  const SecondRoute = () => (
+    <List.Section>
+        <List.Subheader>Friday, May 8th</List.Subheader>
+        <List.Item
+            title="CSE 403 GROUP(group name)"
+        />
+        <List.Item
+            title="Team Meeting(meeting name)"
+        />
+        <List.Item
+            title="3:00 PM - 7:00 PM"
+        />
+    </List.Section>      
+  );
+
 function MeetingsScreen({ navigation }) {
+
+    const [index, setIndex] = React.useState(0);
+    const [routes] = React.useState([
+        { key: 'first', title: 'CONFIRMED' },
+        { key: 'second', title: 'TENTATIVE' },
+    ]);
+    const renderScene = SceneMap({
+        first: FirstRoute,
+        second: SecondRoute,
+    });
+
     return (
         <View style={styles.container}>
           <Appbar.Header style={styles.navbar} >
@@ -20,36 +61,12 @@ function MeetingsScreen({ navigation }) {
               onPress={()=> alert("Will eventually take you to the settings screen")}
             />
           </Appbar.Header>
-
-
-        <List.Section>
-            <List.Subheader>Tuesday, May 5th</List.Subheader>
-            <List.Item
-            title="CSE 403 GROUP(group name)"
-            />
-            <List.Item
-            title="Team Meeting(meeting name)"
-            />
-            <List.Item
-            title="3:00 PM - 7:00 PM"
-            />
-        </List.Section>
-         
-
-        <List.Section>
-        <List.Subheader>Friday, May 8th</List.Subheader>
-        <List.Item
-            title="CSE 403 GROUP(group name)"
-        />
-        <List.Item
-            title="Team Meeting(meeting name)"
-        />
-        <List.Item
-            title="3:00 PM - 7:00 PM"
-        />
-        </List.Section>
         
-          
+          <TabView
+            navigationState={{ index, routes }}
+            renderScene={renderScene}
+            onIndexChange={setIndex}
+          />
         </View>
     );
 }
