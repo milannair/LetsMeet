1c15073cdb633fd256c5b7bd62f5cd9b7b526abf
ratanap <ratanap@uw.edu>
2020-05-21 16:23:35 -0700

    Added skeleton for MeetingRequestController and OptionController

---
+++ frontend/controllers/OptionsController.js
@@ -0,0 +1,6 @@
+const axios = require('axios').default;
+const protocol = 'http://';
+const baseUrl = '127.0.0.1';
+const port = 8000;
+const route = '/lm';
+const url = protocol + baseUrl + ':' + port + route
