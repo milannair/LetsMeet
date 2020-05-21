33b970bf25c59c875f39827ccc6acd63c78a7ddf
Milan Nair <milannair99@gmail.com>
2020-05-21 15:18:35 -0700

    added temporary peer key so this branch can be merged onto master

--- init.sh
+++ init.sh
@@ -12,7 +12,7 @@ else
     echo 'DB_USERNAME=dev' >> .env
     echo 'DB_PW=devpass' >> .env
     echo 'USE_LOCAL_DB=false' >> .env
-    echo 'privateKey=' >> .env
+    echo 'privateKey=peerKey' >> .env
     npm install
     cd ../frontend
     npm install
