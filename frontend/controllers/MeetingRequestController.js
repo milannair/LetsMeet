a37d62f8bd86aa3eb0f2f69c642dda0b3dc9b10c
ratanap <ratanap@uw.edu>
2020-05-21 19:32:40 -0700

    added one small function to meeting request controller

--- frontend/controllers/MeetingRequestController.js
+++ frontend/controllers/MeetingRequestController.js
@@ -4,3 +4,21 @@ const baseUrl = '127.0.0.1';
 const port = 8000;
 const route = '/lm';
 const url = protocol + baseUrl + ':' + port + route
+
+// export async function createGroupMeetingRequest(author, groupId, name, isUnanimousMeetingRequest,
+//     requestedOptions, deadline, status) {
+
+//     }
+
+async function createMeetingRequest(author, groupId, name, isUnanimousMeetingRequest,
+    requestedOptions, deadline, status) {
+
+    try{
+        return response = (await axios.post(url + '/meetingRequest/' + author + '&'+
+        groupId + '&' + name + '&' + isUnanimousMeetingRequest + '&' + requestedOptions + '&' + 
+        deadline + '&' + status)).data;
+    } catch(err) {
+        console.log(err)
+    }
+
+}
