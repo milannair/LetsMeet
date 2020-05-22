82d67e9a77832dcf8b4c33eec72c5caab22fcaf6
Leo <liaoyuan@gmail.com>
2020-05-21 17:57:07 -0700

    Modify back-end test for new API changes

--- api/__tests__/user.test.js
+++ api/__tests__/user.test.js
@@ -1,69 +1,76 @@
 // Tests for the /user and /users endpoints
 
-const constants = require('./ignored/constants');
-const axios = require('axios').default;
+const constants = require("./ignored/constants");
+const axios = require("axios").default;
 
-describe('/user/meetings/:userId', () => {
-  it('gets 404 status when invalid user is given', async () => {
-    const response = await axios.get(
-      constants.API_URI + '/user/meetings/' + constants.FAKE_OBJECT_ID
-    );
-    expect(response.data.status).toBe(404);
-  });
-});
-
-describe('/user/addMeeting', () => {
-  it('removes a meeting from a user', async () => {
-    let userId;
+describe("/user/meetings/:userId", () => {
+  it("gets 404 status when invalid user is given", async () => {
     try {
-      const meetingId = constants.FAKE_OBJECT_ID
-
-      const userRes = await axios.post(constants.API_URI + '/users', {
-        username: 'testuser',
-        email: 'test@example.com',
-        phone: '123456789',
-        password: 'DontUseThisPassword',
-        displayName: 'TestUser',
-      });
-      userId = userRes.data.data._id;
-      await axios.post(constants.API_URI + '/user/addMeeting', {
-        userId: userId,
-        meetingId: meetingId,
-      });
-      const response = await axios.get(constants.API_URI + '/user/' + userId);
-      expect(response.data.data.meetings).toContain(meetingId);
-    } finally {
-      // TODO: tear down with REST API when user deletion endpoint is available
+      await axios.get(
+        `${constants.API_URI}/user/meetings/${constants.FAKE_OBJECT_ID}`
+      );
+    } catch (err) {
+      // Errors are expected
+      expect(err.response.status).toBe(404);
     }
   });
 });
 
-describe('/user/removeMeeting', () => {
-  it('adds a meeting to a user', async () => {
+describe("/user/addMeeting", () => {
+  it("adds a meeting to a user", async () => {
     let userId;
     try {
-      const meetingId = constants.FAKE_OBJECT_ID
+      const meetingId = constants.FAKE_OBJECT_ID;
 
-      const userRes = await axios.post(constants.API_URI + '/users', {
-        username: 'testuser',
-        email: 'test@example.com',
-        phone: '123456789',
-        password: 'DontUseThisPassword',
-        displayName: 'TestUser',
-      });
-      userId = userRes.data.data._id;
-      await axios.post(constants.API_URI + '/user/addMeeting', {
-        userId: userId,
-        meetingId: meetingId,
-      });
-      await axios.post(constants.API_URI + '/user/removeMeeting', {
-        userId: userId,
-        meetingId: meetingId,
-      });
-      const response = await axios.get(constants.API_URI + '/user/' + userId);
-      expect(response.data.data.meetings).toHaveLength(0);
+      const userRes = await axios.post(
+        `${constants.API_URI}/user/create`,
+        {
+          username: "testuser",
+          email: "test@example.com",
+          phone: "1234567890",
+          password: "DontUseThisPassword",
+          displayName: "TestUser",
+        }
+      );
+      userId = userRes.data._id;
+      await axios.post(
+        `${constants.API_URI}/user/addMeeting/${userId}/${meetingId}`
+      );
+      const response = await axios.get(`${constants.API_URI}/user/${userId}`);
+      expect(response.data.meetings).toContain(meetingId);
     } finally {
-      // TODO: tear down with REST API when user deletion endpoint is available
+      axios.delete(`${constants.API_URI}/user/${userId}`);
+    }
+  });
+});
+
+describe("/user/removeMeeting", () => {
+  it("removes a meeting from a user", async () => {
+    let userId;
+    try {
+      const meetingId = constants.FAKE_OBJECT_ID;
+
+      const userRes = await axios.post(
+        `${constants.API_URI}/user/create`,
+        {
+          username: "testuser",
+          email: "test@example.com",
+          phone: "1234567890",
+          password: "DontUseThisPassword",
+          displayName: "TestUser",
+        }
+      );
+      userId = userRes.data._id;
+      await axios.post(
+        `${constants.API_URI}/user/addMeeting/${userId}/${meetingId}`
+      );
+      await axios.post(
+        `${constants.API_URI}/user/removeMeeting/${userId}/${meetingId}`
+      );
+      const response = await axios.get(`${constants.API_URI}/user/${userId}`);
+      expect(response.data.meetings).toHaveLength(0);
+    } finally {
+      axios.delete(`${constants.API_URI}/user/${userId}`);
     }
   });
 });
