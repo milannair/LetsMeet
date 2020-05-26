48da6cff66a65e53d2db3c936c4e2bc1efd6fee6
ratanap <ratanap@uw.edu>
2020-05-26 16:04:13 -0700

    changed card size

--- frontend/components/GroupCardComponent/styles.js
+++ frontend/components/GroupCardComponent/styles.js
@@ -2,7 +2,7 @@ import { StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-      height: '85px',
+      height: 75,
       width: '100%',
       alignItems: 'center',
       justifyContent: 'center'
