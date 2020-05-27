44adea3be08be95b1b1934badc87092f26327c5e
Brandon Fok <fokb@uw.edu>
2020-05-26 22:50:55 -0700

    add file for global api url for frontend controllers and socket handler

---
+++ frontend/api-routes.js
@@ -0,0 +1,6 @@
+export const protocol = 'http://';
+export const baseUrl = '9bda9754.ngrok.io';
+export const port = 8000;
+export const route = '/lm';
+export const url = protocol + baseUrl+ route;
+export const socketUrl = protocol + baseUrl;
