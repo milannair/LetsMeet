4a1d72439e819d6753be30f85f12b35bf0d7a837
Leo <liaoyuan@gmail.com>
2020-06-01 11:08:34 -0700

    Add front-end group invitation model and controller

---
+++ frontend/controllers/GroupInvitationController.js
@@ -0,0 +1,111 @@
+import { url } from '../api-routes';
+const axios = require('axios').default;
+import GroupInvitation from "../models/GroupInvitation";
+
+/*
+ * Returns the array of pending group invitations for a user represented by
+ * 'GroupInvitation' objects, or 'null' if an error occurred.
+ */
+export async function getUserGroupInvitations(userId) {
+  let invitations = [];
+  try {
+    const groupIds = await getGroupRequests(userId);
+    for (const groupId of groupIds) {
+      const groupName = await getGroupName(groupId);
+      invitations.push(new GroupInvitation(groupId, groupName));
+    }
+    return invitations;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+/*
+ * Accept a pending group invitation for a user. Returns server's response for
+ * the accept action, or 'null' if an error occurred.
+ */
+export async function acceptGroupInvitation(userId, groupId) {
+  try {
+    await addUserToGroup(userId, groupId);
+    return await removeGroupRequest(userId, groupId);
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+/*
+ * Decline a pending group invitation for a user. Returns server's response for
+ * the decline action, or 'null'  if an error occurred.
+ */
+export async function declineGroupInvitation(userId, groupId) {
+  try {
+    return await removeGroupRequest(userId, groupId);
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+// Helper Functions
+
+/*
+ * Returns the array of IDs for the groups that the user has been invited to
+ * join. An exception will be thrown if an error occurred.
+ */
+async function getGroupRequests(userId) {
+  const response = await axios.get(`${url}/user/${userId}`);
+  if (response.status === 200) {
+    return response.data.requests;
+  } else {
+    throw response;
+  }
+}
+
+/*
+ * Returns the name of the group with the specified group ID. An exception will
+ * be thrown if an error occurred.
+ */
+async function getGroupName(groupId) {
+  const response = await axios.get(`${url}/group/name/${groupId}`);
+  if (response.status === 200) {
+    return response.data.data.name; // TODO: change this if API code is touched
+  } else {
+    throw response;
+  }
+}
+
+/*
+ * Adds a user to a group. An exception will be thrown if an error occurred.
+ */
+async function addUserToGroup(userId, groupId) {
+  const userResponse = await axios.post(
+    `${url}/user/addGroup/${userId}&${groupId}`);
+  if (userResponse.status !== 200) {
+    throw userResponse;
+  }
+  const groupResponse = await axios.post(
+    `${url}/group/addMemeber`, // TODO: typo in API route
+    {
+    userId: userId,
+    groupId: groupId,
+  });
+  if (groupResponse.status !== 200) {
+    throw groupResponse;
+  }
+}
+
+/*
+ * Removes a group invitation for a user, and returns the server's response.
+ * An exception will be thrown if an error occurred.
+ */
+async function removeGroupRequest(userId, groupId) {
+  const response = await axios.post(
+    `${url}/user/removeGroupRequest/${userId}&${groupId}`);
+  if (response.status === 200) {
+    return response.data;
+  } else {
+    throw response;
+  }
+}
