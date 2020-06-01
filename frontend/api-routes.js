eb72e9545e0eadd1a576ac847a0d7900c7fe4b55
Admin <nelson.tan7@gmail.com>
2020-06-01 14:04:05 -0700

    change api routes back to normal

--- frontend/api-routes.js
+++ frontend/api-routes.js
@@ -1,6 +1,6 @@
 export const protocol = 'http://';
-export const baseUrl = '2dfa99ae702b.ngrok.io';
+export const baseUrl = 'localhost';
 export const port = 8000;
 export const route = '/lm';
-export const url = protocol + baseUrl + route;
-export const socketUrl = protocol + baseUrl;
+export const url = protocol + baseUrl + ':' + port + route;
+export const socketUrl = protocol + baseUrl + ':' + port;
