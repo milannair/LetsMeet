cd0b3bd5db105b26e37b817a975b9ebf01886a0f
Brandon Fok <fokb@uw.edu>
2020-06-02 16:04:11 -0700

    remove warning messages on mobile with websockets

--- frontend/App.js
+++ frontend/App.js
@@ -3,6 +3,11 @@ import { StyleSheet, View } from 'react-native';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import Constants from 'expo-constants';
 import Navigator from './navigation/index';
+console.ignoredYellowBox = ['Remote debugger'];
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings([
+    'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
+]);
 
 const theme = {
   ...DefaultTheme,
