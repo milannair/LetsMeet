765655e6e19a72135a56013f4f2ecfcba36debc1
Admin <nelson.tan7@gmail.com>
2020-06-02 14:36:48 -0700

    api-routes

--- frontend/api-routes.js
+++ frontend/api-routes.js
@@ -1,6 +1,6 @@
 export const protocol = 'http://';
-export const baseUrl = 'ccb43e3f2412.ngrok.io';
+export const baseUrl = '35.212.154.81';
 export const port = 8000;
 export const route = '/lm';
-export const url = protocol + baseUrl + route;
-export const socketUrl = protocol + baseUrl;
+export const url = protocol + baseUrl + ":" + port + route;
+export const socketUrl = protocol + baseUrl + ":" + port;
