6fa4627ee843b94ad32d02f90ea785ca795481ee
Admin <nelson.tan7@gmail.com>
2020-06-02 01:12:01 -0700

    no meeting requests message

--- frontend/components/ViewGroupComponent/index.js
+++ frontend/components/ViewGroupComponent/index.js
@@ -10,6 +10,9 @@ function ViewGroupComponent({route, navigation, updateLog, logData, groupData}) 
     
     useEffect(() => {
         let list = [];
+        if (logData.length === 0) {
+            list.push(<Text>You don't have any meeting requests. Tap on the floating + button to create a meeting request.</Text>)
+        }
             for(let i=0; i < logData.length; i++) {
                 let data = logData[i];
                 let date = new Date(data.meetingRequest.deadline);
